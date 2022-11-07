ff26e141cfd9ce1fa5d469f6b458fde83c1e388d
Camila.Tinelli <cftinelli@outlook.com>
2022-11-07 16:53:55 -0300

    setima aula

---
+++ digital-house/src/aulas/SetimoAula/index.jsx
@@ -0,0 +1,25 @@
+import './style.scss'
+
+
+export function SetimaAula(){
+
+
+    return (
+
+        <main className="setima-aula-component">
+
+            <h1>Contador</h1>
+
+            <button>Adicionar</button>
+            <span>Numero atual: 0</span>
+
+
+
+
+        </main>
+
+
+
+
+    )
+}
