e5ce59faded765d2ee259952aee6231cfbb02290
Camila.Tinelli <cftinelli@outlook.com>
2022-11-07 09:34:04 -0300

    segunda aula

--- digital-house/src/aulas/SegundaAula/index.jsx
+++ digital-house/src/aulas/SegundaAula/index.jsx
@@ -2,12 +2,13 @@ import './style.scss'
 
 export function SegundaAula (){
     return (
-        <main>
+        <main className='segunda-aula-main'>
             <h1>Segunda Aula</h1>
+            <h2>Projeto escolhido: facebook</h2>
             <ul>
                 <li>
                     <h1>Primeiro Componente</h1>
-                    <p>Facebook</p>
+                    <p></p>
                 </li>
             </ul>
         </main>
