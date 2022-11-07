9ada590ee2bec5578b614e85c4f054deffc4d5e3
Camila.Tinelli <cftinelli@outlook.com>
2022-11-07 16:08:41 -0300

    quinta aula

--- digital-house/src/aulas/QuintaAula/index.jsx
+++ digital-house/src/aulas/QuintaAula/index.jsx
@@ -1,11 +1,55 @@
-
+import './style.scss'
+import youtubeImage from './../../assets/images/youtube.jpg'
+import { QuintaAulaItem } from '../../components/QuintaAulaItem'
 
 
 export function QuintaAula(){
 
-    return(
-        <>
-        </>
+
+    const componentsFinded = [
+
+        { 
+            title: 'Video Component', 
+            image: youtubeImage, 
+            text:'Lorem ipsum'
+        },
+        { 
+            title: 'Input Component', 
+            image: youtubeImage, 
+            text:'Lorem ipsum'
+        }
+
+    ]
+
+
+
+
+
+    return (
+        <div className='quarta-aula-component'>
+
+            {/* <QuintaAulaItem title='Primeiro Componente'/> */}
+
+            <h1 className='main-title'>Componentes identificados</h1>
+
+            <ul className = 'components-finded'>
+
+                {
+                    componentsFinded.map(
+                        component => {
+                            return (
+                                <QuintaAulaItem item={component}/>
+                            )
+                        }
+
+                    )
+                }
+
+              
+            </ul>
+        </div>
+        
+        
     )
 
 }
